a3776fe5d09c4512bb758467d63029d706c8cce5
Renisson Silva <renissonx@gmail.com>
2024-09-18 23:57:19 -0300

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    './src/app/page.tsx',
-    './src/app/layout.tsx',
-    './src/app/components/itemForm.js',
+    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
