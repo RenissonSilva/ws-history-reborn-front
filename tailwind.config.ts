3a3f42591a2689a2bf7f8c3fe74c1f955108944f
Renisson Silva <renissonx@gmail.com>
2024-09-19 01:19:41 -0300

    Removendo content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,4 @@
 module.exports = {
-  content: [
-    "./Components/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
   purge: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
