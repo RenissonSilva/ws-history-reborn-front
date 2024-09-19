8d33882b0bccee502d2959745e0eee9b2e1de3bf
Renisson Silva <renissonx@gmail.com>
2024-09-18 23:37:37 -0300

    Adicionando pasta components no tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}', // Caminho para os arquivos que o Tailwind deve observar
+    './src/**/*.{js,ts,jsx,tsx}', 
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {},
