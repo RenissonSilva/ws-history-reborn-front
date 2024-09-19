ec19047b562eecfae9a7164d50230b90606daa56
Renisson Silva <renissonx@gmail.com>
2024-09-18 23:14:39 -0300

    Alterando nome da var da api

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Defina a URL base com a porta que você deseja usar
 const api = axios.create({
-  baseURL: process.env.BASE_URL ?? 'http://localhost:3000', // Substitua pela URL e porta desejada
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'https://ws-history-reborn-back.vercel.app', // Substitua pela URL e porta desejada
 });
 
 export default api;
