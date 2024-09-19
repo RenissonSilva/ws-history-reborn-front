e5078eabe4fecb2b967de377e973122616eb6dc1
Renisson Silva <renissonx@gmail.com>
2024-09-18 23:42:12 -0300

    Adicionando arquivos tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}', 
-    './components/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/app/page.tsx',
+    './src/app/layout.tsx',
+    './src/app/components/itemForm.js',
   ],
   theme: {
     extend: {},
